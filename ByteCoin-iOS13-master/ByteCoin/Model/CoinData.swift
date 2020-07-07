1992735bfb8bcb04e6aac0ff65255d614c3a2232
Yixing-Lu <yixinglu97@gmail.com>
2020-07-07 12:49:52 -0700

    first commit

---
+++ ByteCoin-iOS13-master/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,12 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Yixing Lu on 7/7/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+struct CoinData: Codable {
+    let rate: Double
+}
